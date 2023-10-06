8c82d6b4b9049edbc8f192167b8dcb61acad54ee
Mateo <mateomautone98@gmail.com>
2023-10-06 15:40:21 -0300

    filtro por generos

--- js/script.js
+++ js/script.js
@@ -10,7 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("btnBuscar").addEventListener("click", () => {
         // console.log(document.getElementById("inputBuscar").value);
         // console.log(pelis.filter((peli) => peli.title.toLowerCase().match(document.getElementById("inputBuscar").value.toLowerCase())))
-        for (peli of pelis.filter((peli) => peli.title.toLowerCase().match(document.getElementById("inputBuscar").value.toLowerCase()))) {
+        document.getElementById("lista").innerHTML = ''
+        let queryBusqueda = document.getElementById("inputBuscar").value.toLowerCase()
+        for (peli of pelis.filter((peli) => peli.title.toLowerCase().match(queryBusqueda) || peli.tagline.toLowerCase().match(queryBusqueda) || peli.overview.toLowerCase().match(queryBusqueda) || peli.genres.map((g) => g.name.toLowerCase()).includes(queryBusqueda))) {
             let estrellitas = ``
             for (let i = 0; i < Math.round(peli.vote_average / 2); i++){
                 estrellitas += `<span class="fa fa-star checked"></span>`
